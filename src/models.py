c6b7a20945c76316736b78f9d8b11a3492f5320e
ashleylem <ashley.lemus@yahoo.com>
2023-01-19 23:50:58 +0000

    added routes for login, signup, wishlist

--- src/models.py
+++ src/models.py
@@ -4,10 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__= "User"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(130), db.ForeignKey("Wishlist.user_id"), primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    username= db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,14 +16,17 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
         
 class Wishlist(db.Model):
     __tablename__= "Wishlist"
+    user_id= db.Column(db.String(130,))
     id =db.Column(db.Integer, primary_key=True)
-    item_name= db.Column(db.String(120), nullable=False)
+    item_name= db.Column(db.String(500), nullable=False)
     item_price= db.Column(db.Integer, nullable=False)
     item_description=db.Column(db.String(500), nullable=False)
     picture_url= db.Column(db.String(500), nullable=False)
