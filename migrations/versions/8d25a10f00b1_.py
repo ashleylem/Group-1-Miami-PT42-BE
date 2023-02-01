e473c4aa8e23232e3707289864242f05908e859b
ashleylem <ashley.lemus@yahoo.com>
2023-02-01 16:17:40 +0000

    fixed migrations

---
+++ migrations/versions/8d25a10f00b1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8d25a10f00b1
+Revises: c4dda8b894ea, cf4cccce01d7
+Create Date: 2023-02-01 16:05:25.780160
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d25a10f00b1'
+down_revision = ('c4dda8b894ea', 'cf4cccce01d7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
